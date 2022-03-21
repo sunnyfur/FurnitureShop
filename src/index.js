012968a6918c5173b20cb64edee35e2014912d01
Polina <sunnyfur9@gmail.com>
2022-03-21 21:35:29 +0500

    add structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import './styles/index.scss';
-import {sayHi, sayBye} from './about.js';
+const test = require('./js/about.js');
+// import {
+//     sayHi,
+//     sayBye
+// } from './js/about.js';
 
-console.log('hello webpack!');
-console.log('hello webpack2!');
+// console.log('hello webpack!');
+// console.log('hello webpack2!');
 
-sayHi('John'); // Hello, John!
-sayBye('John'); // Bye, John!
+// sayHi('John'); // Hello, John!
+// sayBye('John'); // Bye, John!
+//2 вариант подключения
+// test.sayHi('John'); // Hello, John!
+// test.sayBye('John'); // Bye, John!
