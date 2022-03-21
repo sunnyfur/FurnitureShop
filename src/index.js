0a9187acf639aa1eab6eb791d980f58f93ee30e5
Polina <sunnyfur9@gmail.com>
2022-03-21 21:03:21 +0500

    init git

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './styles/index.scss';
+import {sayHi, sayBye} from './about.js';
+
+console.log('hello webpack!');
+console.log('hello webpack2!');
+
+sayHi('John'); // Hello, John!
+sayBye('John'); // Bye, John!
