18dbdbe36d036272eef99b1aad2c1c20d78508e4
Polina <sunnyfur9@gmail.com>
2022-03-27 20:23:07 +0500

    add require cart

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './styles/index.scss';
 // const lists = require("./js/products.js");
 const addCards = require("./js/allProductsHtml.js");
 addCards.GenerateCards();
-
+require('./js/cart');
 const slider = require("./js/slider.js");
 slider.SliderHeaderBlock();
 slider.SliderBeautifulBlock();
